9978fb481a64af324ed830c950e54bf556306cb9
Cheang Yu Xiang <cheangyxiang@gmail.com>
2025-04-17 02:31:46 +0800

    Fix

--- XGame/game.py
+++ XGame/game.py
@@ -23,10 +23,10 @@ jumpcount = 8
 Isjump = False
 
 
-char_img = pygame.image.load("Character/character.png")  
+char_img = pygame.image.load("CSP1123-TT3L-3-11/Character/character.png")  
 char_img = pygame.transform.scale(char_img, (64, 64))
 
-char_back_img = pygame.image.load("Character/char.back.png")  
+char_back_img = pygame.image.load("CSP1123-TT3L-3-11/Character/char.back.png")  
 char_back_img = pygame.transform.scale(char_back_img, (64, 64))
 
 pygame.display.set_caption("MMU X Secret")
@@ -34,18 +34,18 @@ screen = pygame.display.set_mode((1280, 720))
 clear_color = (30, 150, 50)
 running = True
 tile_kinds = [
-    TileKind("dirt", "MAP/images/dirt.png", False), # 0
-    TileKind("grass", "MAP/images/grass.png", False), # 1
-    TileKind("road", "MAP/images/road.png", False), # 2
-    TileKind("longkang", "MAP/images/longkang.png", False), # 3 
-    TileKind("stairL", "MAP/images/stairL.png", False), # 4
-    TileKind("stairM", "MAP/images/stairM.png", False), # 5
-    TileKind("stairR", "MAP/images/stairR.png", False), # 6
-    TileKind("blueWall", "MAP/images/blueWall.png", False), # 7
-    TileKind("whiteWall", "MAP/images/whiteWall.png", False) # 8
+    TileKind("dirt", "CSP1123-TT3L-3-11/MAP/images/dirt.png", False), # 0
+    TileKind("grass", "CSP1123-TT3L-3-11/MAP/images/grass.png", False), # 1
+    TileKind("road", "CSP1123-TT3L-3-11/MAP/images/road.png", False), # 2
+    TileKind("longkang", "CSP1123-TT3L-3-11/MAP/images/longkang.png", False), # 3 
+    TileKind("stairL", "CSP1123-TT3L-3-11/MAP/images/stairL.png", False), # 4
+    TileKind("stairM", "CSP1123-TT3L-3-11/MAP/images/stairM.png", False), # 5
+    TileKind("stairR", "CSP1123-TT3L-3-11/MAP/images/stairR.png", False), # 6
+    TileKind("blueWall", "CSP1123-TT3L-3-11/MAP/images/blueWall.png", False), # 7
+    TileKind("whiteWall", "CSP1123-TT3L-3-11/MAP/images/whiteWall.png", False) # 8
 ]
-map = Map("MAP/maps/start.map", tile_kinds, 32)
-Sprite("MAP/images/cat.png", 10 * 32, 15 * 32)
+map = Map("CSP1123-TT3L-3-11/MAP/maps/start.map", tile_kinds, 32)
+Sprite("CSP1123-TT3L-3-11/MAP/images/cat.png", 10 * 32, 15 * 32)
 
 # Game loop
 while running:
