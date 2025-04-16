1a9bcd2c1ecbfb6f3201c2cc82d5e12d72010661
Cheang Yu Xiang <cheangyxiang@gmail.com>
2025-04-16 23:04:43 +0800

    DEMO play

--- Character/mygame2.py
+++ Character/mygame2.py
@@ -7,61 +7,42 @@ screen_height=720
 screen=pygame.display.set_mode((screen_width,screen_height))
 
 def up_edge():
-    return y >=radius
+    return y >=64
 def down_edge():
-    return y<=screen_height-radius
+    return y<=screen_height
 def left_edge():
-    return x>=0+radius
+    return x>=64
 def right_edge():
-    return x<=screen_width-radius
+    return x<=screen_width
 
 x=725
 y=375
-radius=50
 speed=7
 
 run = True
-run2 = False
-
-while run:
-    key = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if key[pygame.K_RETURN]:
-            run = False
-            run2 = True
-        if event.type == pygame.QUIT:
-            run = False
-    screen.fill((0,0,0))
-    pygame.display.flip()
-
-
-
-screen2 = pygame.display.set_mode((screen_width,screen_height))
 
 jumpcount = 10
 Isjump = False
 
 
-char_img = pygame.image.load("character.png")  
+char_img = pygame.image.load("CSP1123-TT3L-3-11/Character/character.png")  
 char_img = pygame.transform.scale(char_img, (64, 64))
 
-char_back_img = pygame.image.load("char.back.png")  
+char_back_img = pygame.image.load("CSP1123-TT3L-3-11/Character/char.back.png")  
 char_back_img = pygame.transform.scale(char_back_img, (64, 64))
 
 
-while run2:
+while run:
     pygame.time.delay(10)
+    key = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT or key[pygame.K_RETURN]:
-            run2 = False
-    screen2.fill((180,220,220))
+            run = False
+    screen.fill((180,220,220))
 
     #screen.blit(char_back_img, (x, y))
 
     screen.blit(char_img, (x, y))
-        
-
-    #pygame.draw.circle(screen,(211,211,211),(x,y),radius)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_a] and left_edge():
---
+++ XGame/game.py
@@ -0,0 +1,91 @@
+import pygame
+from sprite import sprites
+from map import TileKind, Map
+
+pygame.init()
+
+def up_edge():
+    return y >=64
+def down_edge():
+    return y <=656
+def left_edge():
+    return x >=64
+def right_edge():
+    return x <=1216
+
+x=725
+y=375
+speed=7
+
+run = True
+
+jumpcount = 8
+Isjump = False
+
+
+char_img = pygame.image.load("CSP1123-TT3L-3-11/Character/character.png")  
+char_img = pygame.transform.scale(char_img, (64, 64))
+
+char_back_img = pygame.image.load("CSP1123-TT3L-3-11/Character/char.back.png")  
+char_back_img = pygame.transform.scale(char_back_img, (64, 64))
+
+pygame.display.set_caption("MMU X Secret")
+screen = pygame.display.set_mode((1280, 720))
+clear_color = (30, 150, 50)
+running = True
+tile_kinds = [
+    TileKind("dirt", "CSP1123-TT3L-3-11/MAP/images/dirt.png", False), #0
+    TileKind("grass", "CSP1123-TT3L-3-11/MAP/images/grass.png", False), #1
+    TileKind("sand", "CSP1123-TT3L-3-11/MAP/images/sand.png", False), #2
+    TileKind("wood", "CSP1123-TT3L-3-11/MAP/images/wood.png", False), #3
+    TileKind("stairL", "CSP1123-TT3L-3-11/MAP/images/stairL.png", False), #4
+    TileKind("stairM", "CSP1123-TT3L-3-11/MAP/images/stairM.png", False), #5
+    TileKind("stairR", "CSP1123-TT3L-3-11/MAP/images/stairR.png", False) #6
+]
+map = Map("CSP1123-TT3L-3-11/MAP/maps/start.map", tile_kinds, 32)
+
+# Game loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Draw Code
+    screen.fill(clear_color)
+    map.draw(screen)
+    for s in sprites:
+        s.draw(screen)
+
+    #screen.blit(char_back_img, (x, y))
+
+    screen.blit(char_img, (x, y))
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] and left_edge():
+        x-=speed
+    if key[pygame.K_d] and right_edge():
+        x+=speed
+    if not(Isjump):
+        if key[pygame.K_w] and up_edge():
+            y-=speed
+        if key[pygame.K_s] and down_edge():
+            y+=speed
+        if key[pygame.K_SPACE]:
+            Isjump = True
+    else:
+        if jumpcount >= -8:
+            neg = 1
+            if jumpcount <0:
+                neg = -1
+            y-=(jumpcount ** 2) * 0.3 * neg
+            jumpcount -=1
+        else:
+            Isjump = False
+            jumpcount = 8
+
+    pygame.display.flip()
+
+    pygame.time.delay(17)
+
+
+pygame.quit()
